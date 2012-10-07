4294de0a7aebd5c1c3d87c6592826b6f4b2ef02c
Student <student@student-VirtualBox.(none)>
2012-10-07 19:36:21 +0200

    First - new gwt project

---
+++ src/new_gwt/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package new_gwt.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
